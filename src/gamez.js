6c6c7f8e00e99111ab323089a0a8b22504ef4dfd
pincesCPU987 <75500175+pincesCPU987@users.noreply.github.com>
2023-10-16 11:01:57 -0700

    Add position rounding to Sprite.Looks.show() so decimal adjustments to position don't cause flickering

--- src/gamez.js
+++ src/gamez.js
@@ -328,11 +328,15 @@ Width and height are (${this.parent.rect.width}, ${this.parent.rect.height}).`)
     var ctx = this.parent.screen.ctx;
     ctx.drawImage(
       this.costumes[this.costume].object,
-      this.parent.rect.x - (
-        this.costumes[this.costume].object.width / 2
+      Math.round(
+        this.parent.rect.x - (
+          this.costumes[this.costume].object.width / 2
+        )
       ),
-      this.parent.rect.y - (
-        this.costumes[this.costume].object.height / 2
+      Math.round(
+        this.parent.rect.y - (
+          this.costumes[this.costume].object.height / 2
+        )
       )
     );
   }
